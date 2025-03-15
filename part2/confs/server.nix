ea7f17ee247f1d32906c61b595de72061e0f5f58
test <test@example.com>
2025-03-15 21:01:25 +0000

    extra_scripting_ressources

--- part2/confs/server.nix
+++ part2/confs/server.nix
@@ -1,10 +1,29 @@
 {config, pkgs, ...}:
 {
+    #NOTE: useful commands for development purpose ----->>>
+    #NOTE:------------------------------------------------------
     environment.systemPackages = with pkgs; [
-        (writeShellScriptBin "kgn" ''
-            sudo k3s kubectl get nodes -o wide
+
+        #NOTE: check status
+        (writeShellScriptBin "kgp" ''sudo k3s kubectl get pods -o wide'')
+        (writeShellScriptBin "kgd" ''sudo k3s kubectl get deployments -o wide'')
+        (writeShellScriptBin "kga" ''sudo k3s kubectl get all -n kube-system'')
+        (writeShellScriptBin "kgn" ''sudo k3s kubectl get nodes -o wide'')
+        (writeShellScriptBin "reb" ''sudo nixos-rebuild switch'')
+
+        #NOTE: troubleshoot
+        (writeShellScriptBin "net_log" ''journalctl -u systemd-networkd --no-pager --since "10 minutes ago"'')
+        (writeShellScriptBin "k_log" ''journalctl -u k3s.service --no-pager --since "1 hour ago"'')
+
+        (writeShellScriptBin "ed" ''vim /etc/nixos/vagrant-provision.nix'')
+
+        #NOTE: fresh start of k3s, resets cluster, turning off all dependencies
+        (writeShellScriptBin "k_reset" ''
+                sudo /vagrant/scripts/reset_cluster.sh
         '')
+
     ];
+    #NOTE:-------------------------------------------------
 
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
---
+++ part2/scripts/monitor_rollout.sh
@@ -0,0 +1,1 @@
+k3s kubectl rollout status deployment <name>
