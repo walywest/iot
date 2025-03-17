9a727035bc23458bf09b02670cf4a238f6065487
test <test@example.com>
2025-03-17 16:55:01 +0000

    nginx-ingress-bug

--- part2/scripts/3apps.sh
+++ part2/scripts/3apps.sh
@@ -3,4 +3,7 @@ sudo k3s kubectl delete deployment,service --all
 sudo k3s kubectl apply -f \
 https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/\
 deploy/static/provider/cloud/deploy.yaml
-sudo k3s kubectl apply -f /vagrant/scripts/app-three.yml /vagrant/scripts/app-two.yml /vagrant/scripts/app-one.yml /vagrant/scripts/ingress-nginx.yml
+sudo k3s kubectl apply -f /vagrant/scripts/app-three.yml
+sudo k3s kubectl apply -f /vagrant/scripts/app-two.yml
+sudo k3s kubectl apply -f /vagrant/scripts/app-one.yml
+sudo k3s kubectl apply -f /vagrant/scripts/ingress-nginx.yml
