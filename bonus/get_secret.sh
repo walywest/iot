8915dc42712c6344c38f82c1037800f4dc214679
test <test@example.com>
2025-04-05 02:10:58 +0000

    cleaning_files

--- bonus/get_secret.sh
+++ bonus/get_secret.sh
@@ -1,5 +1,5 @@
-username is admin
-2g2nv7oQgiXRL2ut
-Get ArgoCD default admin password
+# username is admin
+# Get ArgoCD default admin password
 kubectl -n argocd get secret argocd-initial-admin-secret \
           -o jsonpath="{.data.password}" | base64 -d; echo
+          # 2g2nv7oQgiXRL2ut
--- bonus/scripts/ressources.sh
+++ bonus/scripts/ressources.sh
@@ -2,7 +2,7 @@ DIR="/home/dumbledoor/Desktop/mynixbox/bonus"
 
 sudo k3d cluster create --config ${DIR}/conf/k3d-cluster.yml
 # #NOTE: deploying argocd
-sudo kubectl apply -f ${DIR}/conf/argocd-conf.yml
+sudo kubectl create namespace dev
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # #NOTE: deploying application ressource with argocd which would automate wil42 deployment and CI
 sudo kubectl apply -f ${DIR}/conf/desired-state.yml
