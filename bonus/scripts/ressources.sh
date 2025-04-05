3044a4965635fbaa34ffe8539146fcfcf0ca8f7d
test <test@example.com>
2025-04-05 01:44:29 +0000

    bonus_working

--- bonus/scripts/ressources.sh
+++ bonus/scripts/ressources.sh
@@ -2,7 +2,7 @@ DIR="/home/dumbledoor/Desktop/mynixbox/bonus"
 
 sudo k3d cluster create --config ${DIR}/conf/k3d-cluster.yml
 # #NOTE: deploying argocd
-# sudo kubectl apply -f ../conf/argocd-conf.yml
-# sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl apply -f ${DIR}/conf/argocd-conf.yml
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # #NOTE: deploying application ressource with argocd which would automate wil42 deployment and CI
-# sudo kubectl apply -f ../conf/desired-state.yml
+sudo kubectl apply -f ${DIR}/conf/desired-state.yml
